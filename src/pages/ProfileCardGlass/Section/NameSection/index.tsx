368bac156807ad863edcdc14fc0201e887136902
Rian <rian.tosm@gmail.com>
2023-09-07 21:06:32 +0700

    current code

--- src/pages/ProfileCardGlass/Section/NameSection/index.tsx
+++ src/pages/ProfileCardGlass/Section/NameSection/index.tsx
@@ -4,24 +4,19 @@ export default function NameSection() {
   return (
     <div className="flex-grow grid items-center h-fit">
       <div className="grid w-full h-full">
-        <div className="place-self-center items-center grid grid-cols-3 px-5 w-full">
-          <div className="text-center">
-            {/* <p className="text-white font-bold text-lg">542</p>
-          <p className="text-slate-300 text-xs">Followers</p> */}
-          </div>
+        <div className="place-self-center items-center grid justify-center px-5 w-full">
           <img
             src={Assets.Images.ProfilePicture}
             alt=""
-            className="w-20 h-20 rounded-full shadow-xl"
+            className="max-w-20 h-20 rounded-full shadow-xl aspect-square object-contain"
           />
-          <div className="text-center">
-            {/* <p className="text-white font-bold text-lg">534</p>
-          <p className="text-slate-300 text-xs">Following</p> */}
-          </div>
         </div>
         <div className="text-center mt-2">
           <p className="text-white text-lg font-bold font-noto">Rian Tosm</p>
-          <p className="text-gray-500 font-normal text-[11px]" style={{ lineHeight: 0.7 }}>
+          <p
+            className="text-gray-500 font-normal text-[11px]"
+            style={{ lineHeight: 0.7 }}
+          >
             Software Engineer
           </p>
         </div>
--- src/pages/ProfileCardGlass/Section/QuoteSection/index.tsx
+++ src/pages/ProfileCardGlass/Section/QuoteSection/index.tsx
@@ -1,11 +1,11 @@
 export default function QuoteSection() {
   return (
     <div
-      className="text-center px-5 place-self-center w-[220px] h-full items-end grid pb-8"
+      className="text-center px-5 place-self-center max-w-[220px] h-full items-end grid pb-4"
       style={{ lineHeight: 1.45 }}
     >
       <span className="text-gray-500 italic font-normal text-xs">
-        I think she doesn't like me
+        A lesson I learned from the gym, pain is important for growth
       </span>
     </div>
   );
